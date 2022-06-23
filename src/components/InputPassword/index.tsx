ee0c3e4b91e80b3c909f330a9a815454e43114f5
christianfdev <chriis_farias@hotmail.com>
2022-06-23 10:39:05 -0300

    Implements auth

--- src/components/InputPassword/index.tsx
+++ src/components/InputPassword/index.tsx
@@ -1,10 +1,12 @@
 import './styles.css';
 
-export const InputPassword = () => (
+export const InputPassword = ({ name, onChange }: any) => (
   <input 
         className="input"
         type="password" 
         placeholder="Senha" 
+        name={name}
+        onChange={onChange}
   />
 )
   
--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -1,10 +1,12 @@
 import './styles.css';
 
-export const InputText = ({ placeholder }: any) => (
+export const InputText = ({ placeholder, onChange, name }: any) => (
   <input 
         className="input"
         type="text" 
+        name={name}
         placeholder={placeholder} 
+        onChange={onChange}
   />
 )
   
